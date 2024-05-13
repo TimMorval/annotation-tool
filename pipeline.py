c6d82ef571b4b1ee9f60ca12c108f5f9dd56605c
timmorval <timothe.morval@gmail.com>
2024-05-13 16:22:46 +0200

    delete pipeline

--- pipeline.py
+++
@@ -1,7 +0,0 @@
-from convert_pdf_to_image import convert_to_images
-from ocr import extract_text_from_images
-from extract_predictions import extract_predictions
-
-convert_to_images("to_convert", "images")
-extract_text_from_images("images", 'ocr_tasks.json')
-extract_predictions("ocr_tasks.json", "predictions")
